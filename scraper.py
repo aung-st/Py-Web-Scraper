fa03074cfd81a99cf78ebf647b08dab225b85a30
Sam Aung <92190177+aung-st@users.noreply.github.com>
2022-06-30 16:48:27 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -19,7 +19,42 @@ def html_code(url):
     #return the lovely html
     return(soup)
 
+def getCustomerName(soup):
+    data_str = ""
+    customer_list = []
+
+    for item in soup.find_all("span",class_ = "a-profile-name"):
+        data_str = data_str + item.get_text()
+        customer_list.append(data_str)
+        data_str = ""
+    return customer_list
+
+def collectReviewData(soup):
+    data_str = ""
+
+    for item in soup.find_all("div",class_ = "a-expander-content reviewText review-text-content a-expander-partial-collapse-content"):
+        data_str = data_str + item.get_text()
+    
+    result = data_str.split("\n")
+    return result
+
+def getCustomerReviews(soup):
+    customer_reviews = collectReviewData(soup)
+    reviews = []
+    for review in customer_reviews:
+        if review == "":
+            pass
+        else:
+            reviews.append(review)
+    return reviews
+
+
 url = "https://www.amazon.co.uk/MusicSafe-Pro-Black-Geh%C3%B6rschutz/dp/B07HHFVSPB/ref=sr_1_6?keywords=alpine+ear+plug&qid=1656601752&sprefix=alpine%2Caps%2C96&sr=8-6"
 
 soup = html_code(url)
-print(soup)
+
+#print a list of names of all customers who left a review
+customer_name = getCustomerName(soup)
+#print(customer_name)
+
+print(getCustomerReviews(soup))
